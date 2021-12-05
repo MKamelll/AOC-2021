3cd8a975a62c03a92d8865be543474a1dacd6f7f
Mina Kamel <minallkamel@gmail.com>
2021-12-05 02:40:47 +0200

    aoc-4_part1

---
+++ aoc-4/aoc4.py
@@ -0,0 +1,98 @@
+def get_order(arr):
+    return [int(i) for i in arr[0].strip("\n").split(",")]
+
+
+def drop_order(arr):
+    return arr[2:]
+
+
+def split_into_boards(arr, boards=None):
+    if not boards: boards = []
+    if "\n" not in arr:
+        index = -1
+    else:
+        index = arr.index("\n")
+
+    if len(arr) < 1 or index == -1:
+        boards.append(arr)
+        return boards
+    
+    boards.append(arr[:index])
+    return split_into_boards(arr[index+1:], boards)    
+
+
+def get_board(board_raw):
+    board = [[(int(j), False) for j in i.split()] for i in board_raw]
+    return board
+
+
+def mark_board(board, num):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j][0] == num:
+                board[i][j] = (board[i][j][0], True)
+                break
+    return board
+
+
+def is_row_complete(board, row_len=5):
+    for i in board:
+        count = len(list(filter(lambda x: x[1] == True, i)))
+        if count == row_len:
+            return True
+    return False
+
+
+def is_col_complete(board, col_len=5):
+    for i in range(col_len):
+        count = len(list(filter(lambda row: row[i][1] == True, board)))
+        if count == col_len:
+            return True
+    return False
+
+
+def unmarked_sum(wining_board):
+    unmarked = []
+    for i in wining_board:
+        unmarked += list(map(lambda x: x[0], filter(lambda x: x[1] == False, i)))
+
+    return sum(unmarked)
+
+
+def score(wining_board, last_matched):
+    return unmarked_sum(wining_board) * last_matched
+
+
+def part_one(boards, order):
+    for num in order:
+        marked_boards = list(map(lambda board: mark_board(board, num), boards))
+        
+        row_completed = list(map(lambda board: is_row_complete(board), marked_boards))
+        if True in row_completed:
+            wining_board = boards[row_completed.index(True)]
+            return score(wining_board, num)
+        
+        col_completed = list(map(lambda board: is_col_complete(board), marked_boards))
+        if True in col_completed:
+            wining_board = boards[col_completed.index(True)]
+            return score(wining_board, num)
+
+
+def main(filename):
+    data = []
+    with open(f"{filename}.txt") as f:
+        for line in f:
+            if len(line) > 1:
+                data += [line.strip("\n").strip(" ")]
+            else:
+                data += [line]
+
+    order = get_order(data)
+    data = drop_order(data)
+    boards_raw = split_into_boards(data)
+    boards = [get_board(i) for i in boards_raw]
+    
+    print(part_one(boards, order))
+
+if __name__ == "__main__":
+    main("input")
